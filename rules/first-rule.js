e401f0834ae22de6687b6d269cab7bfa87bb66fc
asalih <ahmet.salih@windowslive.com>
2016-08-01 14:43:58 +0300

    adding

---
+++ rules/first-rule.js
@@ -0,0 +1,15 @@
+if(args.basket.totalPrice < 0.1){
+    result.errors.push("price cannot be zero or negative!");
+    result.hasError = true;
+}
+
+
+if(args.customer.customerEmail == "xyz@abc.com" || args.customer.customerEmail == "xyza@abc.com"){
+    result.errors.push("unexpected email!")
+    result.hasError = true;
+}
+
+if(args.basket.totalPrice > 1000){
+    result.errors.push("risk!");
+    result.hasError = true;
+}
